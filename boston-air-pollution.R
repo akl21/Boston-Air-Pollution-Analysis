fbc4c6bf8da8cd69d23f03a907e20d3f217bd083
akl21 <anne.lott@duke.edu>
2022-05-30 13:20:34 -0400

    Updated code and HTML RMarkdown file

--- boston-air-pollution.R
+++ boston-air-pollution.R
@@ -12,10 +12,11 @@ df$lstat.med = ifelse(df$lstat > median(df$lstat), "Above Median",
 
 #make a box plot of the air pollution by socioeconomic status
 ggplot(df)+
-  geom_boxplot(aes(x = lstat.med, y = nox), fill = "blue") +
+  geom_boxplot(aes(x = lstat.med, y = nox, fill = lstat.med)) +
   labs(x = "Low Socioeconomic Status Percentage", 
        y = "Nitrogen Oxide Pollution",
-       title = "Boston Air Pollution by Socioeconomic Status")
+       title = "Boston Air Pollution by Socioeconomic Status Box Plot") +
+  theme(legend.position = "none")
 
 #compare mean air pollution by socioeconomic status category of 
 #below or above median
@@ -25,3 +26,10 @@ mean(df$nox[df$lstat.med == "Below Median"])
 #conduct t-test of the two groups
 t.test(nox ~ lstat.med, data = df)
 #t test is highly significant
+
+#make a scatter plot of air pollution by socioeconomic status
+ggplot(df) +
+  geom_point(aes(x = lstat, y = nox), color = "blue") + 
+  labs(x = "Low Socioeconomic Status Percentage",
+       y = "Nitrogen Oxide Pollution Concentration", 
+       title = "Boston Air Pollution by Socioeconomic Status Scatter Plot")
